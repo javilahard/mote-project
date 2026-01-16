8926aa529e2b55c4037334e5a8891023dcd0a2fc
João Avila Harduin <javilahard@gmail.com>
2026-01-16 18:47:10 -0300

    chore: initializes the backend and frontend structures

---
+++ mote-backend/mote-backend/src/main/java/com/mote/backend/MoteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mote.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoteBackendApplication.class, args);
+	}
+
+}
